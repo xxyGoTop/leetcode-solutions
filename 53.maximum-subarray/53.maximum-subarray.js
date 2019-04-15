3edfa4c5c3dbc6c5602564c4b1e7da438bcdce4e
CodeGrocery <2724001387@qq.com>
2019-04-15 11:34:25 +0800

    Add + 增加53.maximum-subarray的获取连续子数组最大和

--- 53.maximum-subarray/53.maximum-subarray.js
+++ 53.maximum-subarray/53.maximum-subarray.js
@@ -33,6 +33,13 @@
  * @return {number}
  */
 var maxSubArray = function(nums) {
-    
+    let sum = 0;
+    let max = -Number.MAX_VALUE;
+
+    for(let i = 0; i < nums.length; i++) {
+      sum = Math.max(sum + nums[i], nums[i])
+      max = Math.max(max, sum)
+    }
+    return max
 };
 
